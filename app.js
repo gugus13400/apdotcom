e82f158a2e62bf402c48b31a39f10160d890769e
Auguste Pugnet <augustepugnet1@gmail.com>
2022-08-25 23:04:06 +0200

    Solid Draft

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
-
-function PageTransitions(section) {
-    // Button click active class
-    for (let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn')
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        });
-    }
-
-    // Section Active Class
-    allSections.addEventListener('click',(e) => {
-        const id = e.target.dataset.id;
-        if(id) {
-            // Remove selected from other btns
-            sectBtns.forEach((btn) => {
-                btn.classList.remove('active');
-            })
-            e.target.classList.add('active');
-
-            // Hide other sections
-            sections.forEach((section) => {
-               section.classList.remove('active');
-            })
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
     });
-}
-
-PageTransitions();
-
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
