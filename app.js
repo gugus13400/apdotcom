05859ab02e9abb016a7d3852223234be58c05691
Auguste Pugnet <augustepugnet1@gmail.com>
2022-08-26 19:14:58 +0200

    MVP Blog content

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
@@ -8,7 +8,7 @@
         })
     });
     [...document.querySelectorAll(".nav-btn")].forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             document.querySelector(".active-nav-btn").classList.remove("active-nav-btn");
             this.classList.add("active-nav-btn");
             document.querySelector(".current-nav-tab").classList.add("sleep-nav-tab")
@@ -21,3 +21,36 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dots");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
