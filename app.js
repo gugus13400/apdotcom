2aef487fcf6b45a2c44213e1d5d9b1044652303f
Auguste Pugnet <augustepugnet1@gmail.com>
2022-08-26 13:11:05 +0200

    Added Navigation In blog section

--- app.js
+++ app.js
@@ -7,6 +7,16 @@
             document.getElementById(button.dataset.id).classList.add("active");
         })
     });
+    [...document.querySelectorAll(".nav-btn")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-nav-btn").classList.remove("active-nav-btn");
+            this.classList.add("active-nav-btn");
+            document.querySelector(".current-nav-tab").classList.add("sleep-nav-tab")
+            document.querySelector(".current-nav-tab").classList.remove("current-nav-tab");
+            document.getElementById(button.dataset.id).classList.add("current-nav-tab");
+            document.getElementById(button.dataset.id).classList.remove("sleep-nav-tab");
+        })
+    });
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
